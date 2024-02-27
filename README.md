9a64e2704825c5f5d0fd9c35e892217e63f6d346
nelson-pechmay <nelson.pechmay@gmail.com>
2024-02-27 12:58:38 +0000

    - update README.md to include some screenshots of the WPF app - Inlcude the source code in c# - add the data directory to store the screenshots

--- README.md
+++ README.md
@@ -5,6 +5,13 @@ This repository is intented to host a WPF "hello world" application implemented 
 A simple WPF has been implemented, which displays in a window the text "Hello there!", this text is updated everytime the *Say Hello* button is pressed. In this way, the text is displayed in different colors chosen randomly.
 
 # Usage
-The simplest way to see it in *action* is to run the **WpfHello.exe** located in ./bin/release/WpfHello.exe.
+The simplest way to see it in *action* is to run the **WpfHello.exe** located in ./bin/Release/WpfHello.exe.
 When the window pops-up, click on the *Say Hello* button to see the changes in color of the "Hello there!" text. Enjoy!
+
+Sample result 1               |  Sample result 2
+:----------------------------:|:--------------------------------------:
+![](data/image001.png)  |  ![](data/image002.png)
+
+# Outlook
+The same random choice can be used to display the text in different colors plus in different languages.
 
---
+++ WpfHello/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random rand;
+        public MainWindow()
+        {
+            InitializeComponent();
+            rand = new Random();
+        }
+
+        private byte[] GetRandomBytes(int n) 
+        {
+            var randomBytes = new byte[n];
+            rand.NextBytes(randomBytes);
+            return randomBytes;
+        }
+
+        private void SayHelloButton_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] rgb = GetRandomBytes(4);
+            var randomColorBrush = new SolidColorBrush(Color.FromArgb(rgb[3], rgb[0], rgb[1], rgb[2]));
+            TextBox1.BorderBrush = randomColorBrush;
+            TextBox1.Foreground = randomColorBrush;
+        }
+    }
+}
